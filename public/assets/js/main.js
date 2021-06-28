28c846a22ed869363bbe85bdcf017834bec0eaa0
root <root@Andy.localdomain>
2021-06-28 14:44:08 +0200

    my favourites draft

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,11 @@
-async function getLakes () {
-  const res = await fetch('/api/lake/all')
-  const lakes = await res.json()
-  lakes.forEach(lake => createCard(lake))
+async function getLakes() {
+  const res = await fetch("/api/lake/all");
+  const lakes = await res.json();
+  lakes.forEach((lake) => createCard(lake));
 }
 
-function createCard (lake) {
-  const card = document.createElement('div')
+function createCard(lake) {
+  const card = document.createElement("div");
   card.innerHTML = `<div class="card">
   <div class="card__img">
     <img src="/assets/img/${lake.filename}" alt="${lake.name}" />
@@ -42,10 +42,59 @@ function createCard (lake) {
     <div class="card__text">
       ${lake.short_description}
     </div>
-  </div>`
+  </div>`;
 
-  const cards = document.querySelector('.cards')
-  cards.append(card)
+  const cards = document.querySelector(".cards");
+  cards.append(card);
 }
 
-getLakes()
+getLakes();
+
+// My Favourite Button - Heart
+// $(".favme").click(function () {
+//   $(this).toggleClass("active");
+// });
+
+// /* when a user clicks, toggle the 'is-animating' class */
+// $(".favme").on("click touchstart", function () {
+//   $(this).toggleClass("is_animating");
+// });
+
+// /*when the animation is over, remove the class*/
+// $(".favme").on("animationend", function () {
+//   $(this).toggleClass("is_animating");
+// });
+
+// function sortList() {
+//   let list, i, switching, b, shouldSwitch;
+//   list = document.getElementsByClassName("cards");
+//   switching = true;
+//   /* Make a loop that will continue until
+//   no switching has been done: */
+//   while (switching) {
+//     // start by saying: no switching is done:
+//     switching = false;
+//     b = list.getElementsByTagName("LI");
+//     // Loop through all list-items:
+//     for (i = 0; i < b.length - 1; i++) {
+//       // start by saying there should be no switching:
+//       shouldSwitch = false;
+//       /* check if the next item should
+//       switch place with the current item: */
+//       // To be changed for distance ranking order
+//       if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+//         /* if next item is alphabetically
+//         lower than current item, mark as a switch
+//         and break the loop: */
+//         shouldSwitch = true;
+//         break;
+//       }
+//     }
+//     if (shouldSwitch) {
+//       /* If a switch has been marked, make the switch
+//       and mark the switch as done: */
+//       b[i].parentNode.insertBefore(b[i + 1], b[i]);
+//       switching = true;
+//     }
+//   }
+// }
